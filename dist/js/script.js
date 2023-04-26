d0b8db1df985aecae7ee57080c213bd61705e5c5
Andrew Savinov <savinov300793@gmail.com>
2023-04-26 22:38:29 +0400

    Home page #1

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.promo-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 700,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+    cssEase: 'linear',
+    appendDots: $('.slick-slider-dots'),
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider-arrow-left.svg"></img></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/slider-arrow-right.svg"></img></button>'
+  });
+});
