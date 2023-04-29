ccfcf03c373cd5cbc47384cd453c61e7c73b3d77
Andrew Savinov <savinov300793@gmail.com>
2023-04-29 09:57:59 +0400

    Home page #2

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,14 @@ $(document).ready(function(){
     infinite: true,
     speed: 700,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 4000,
     fade: true,
     cssEase: 'linear',
     appendDots: $('.slick-slider-dots'),
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider-arrow-left.svg"></img></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/slider-arrow-right.svg"></img></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/slider-arrow-right.svg"></img></button>',
   });
+  
+  new WOW().init();
+
 });
